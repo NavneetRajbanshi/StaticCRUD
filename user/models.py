88d01f6c2b97b597acde8eadcdc5ce36779e07cb
NavneetRajbanshi <navneetrajbanshi85@gmail.com>
2021-06-23 12:23:05 +0545

    Static CRUD

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=10)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.name
--- userCRUD/settings.py
+++ userCRUD/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "user",
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
@@ -137,3 +137,8 @@ STATIC_URL = "/static/"
 # manually added
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+
+MEDIA_URL = "/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "images")
