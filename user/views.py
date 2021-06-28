284104e855177a32c9d34a660d8e3d8ff47b81e3
NavneetRajbanshi <navneetrajbanshi85@gmail.com>
2021-06-28 08:24:52 +0545

    Static CRUD

--- user/views.py
+++ user/views.py
@@ -5,36 +5,6 @@ from django.views.generic.base import TemplateView
 class UserAdd(TemplateView):
     template_name = "useradd.html"
 
-    """
-      if request.method == "POST":
-            useraddform = UserAddForm(request.POST)
-            if useraddform.is_valid():
-
-                useradded = useraddform.save(commit=False)
-
-                password = useraddform.cleaned_data["password"]
-                useradded.set_password(useradded.password)
-                useradded.save()
-                """
-                fname = useraddform.cleaned_data["name"]
-                faddress = useraddform.cleaned_data["address"]
-                femail = useraddform.cleaned_data["email"]
-                fpassword = useraddform.cleaned_data["password"]
-                useradd = User(
-                    name=fname, address=faddress, email=femail, password=fpassword
-                )
-                useradd.save()
-                """
-            else:
-                print("invalid form")
-        else:
-            useraddform = UserAddForm()
-    else:
-        return HttpResponseRedirect("../login/")
-
-    return render(request, "useradd.html", {"form": useraddform})
-
-"""
 
 class UserView(TemplateView):
     template_name = "userread.html"
