6fef0eb88365865706c2c5b738fdeaa44191b9a2
NavneetRajbanshi <navneetrajbanshi85@gmail.com>
2021-06-30 10:23:10 +0545

    Static CRUD

--- category/urls.py
+++ category/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 app_name = "category"
 urlpatterns = [
-    path("categoryadd/", views.CategoryAdd.as_view(), name="categoryadd"),
-    path("categoryread/", views.CategoryView.as_view(), name="categoryread"),
+    path("categoryadd/", views.add_category, name="categoryadd"),
+    path("categoryread/", views.show_category, name="categoryread"),
+    path(
+        "deletecategory/<int:id>", views.delete_category, name="deletecategory"
+    ),
+    path("<int:id>", views.update_category, name="updatecategory"),
 ]
